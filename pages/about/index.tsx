917d80ecbde4161a89d8abd76e1a3ea3bd81b110
Mohammed Akmal <meshoronaldo62@gmail.com>
2024-01-07 12:10:36 +0200

    test

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -202,7 +202,7 @@ const About = () => {
                 >
                   {items.title}
                 </div>
-              );
+              ); 
             })}
           </div>
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
