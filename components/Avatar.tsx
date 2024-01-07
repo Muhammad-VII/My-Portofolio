ae8caa42778d660dd812be58065d590acb0df917
Mohammed Akmal <meshoronaldo62@gmail.com>
2024-01-07 13:20:04 +0200

    added metadata

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Avatar = () => {
   return <div className="hidden xl:flex xl:max-w-none">
-    <Image src={'/avatar.png'} width={737} height={737} alt="avatar" className="translate-z-0 w-full h-full"/>
+    <Image priority src={'/avatar.png'} width={737} height={737} alt="avatar" className="translate-z-0 w-full h-full"/>
   </div>;
 };
 
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,22 +1,32 @@
-import { Sora } from 'next/font/google'
+import { Sora } from "next/font/google";
+import type { Metadata } from "next";
 
 // font settings
 const sora = Sora({
-  subsets: ['latin'],
-  variable: '--font-sore',
-})
+  subsets: ["latin"],
+  variable: "--font-sore",
+});
 
-import Nav from './Nav';
-import Header from './Header';
-import TopLeftImg from './TopLeftImg';
+import Nav from "./Nav";
+import Header from "./Header";
+import TopLeftImg from "./TopLeftImg";
 
-const Layout = ({children }: { children: React.ReactNode }) => {
-  return <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
-    <TopLeftImg />
-    <Nav />
-    <Header />
-    {children}
-  </div>;
+export const metadata: Metadata = {
+  title: "Akmal Portfolio",
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://my-portofolio-3scizhrwo-mohammed-akmals-projects.vercel.app/work"),
+};
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
