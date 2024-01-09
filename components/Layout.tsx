27b038607ff711ae772c7f8a81e820c29ea0e201
Mohammed Akmal <meshoronaldo62@gmail.com>
2024-01-09 23:08:54 +0200

    'added_meta_tags'

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import { Sora } from "next/font/google";
-import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 
 // font settings
@@ -11,19 +10,21 @@ const sora = Sora({
 import Nav from "./Nav";
 import Header from "./Header";
 import TopLeftImg from "./TopLeftImg";
+import Head from "next/head";
 
-export const metadata: Metadata = {
-  title: "Akmal Portfolio",
-  description: "The official Next.js Course Dashboard, built with App Router.",
-  metadataBase: new URL(
-    "https://my-portofolio-five-plum.vercel.app/"
-  ),
-};
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
       className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
     >
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="Akmal's Portfolio" />
+        <meta name="keywords" content="Akmal, Portfolio, Web Developer" />
+        <meta name="author" content="Mohmmed Akmal" />
+        <title>Akmal's Portfolio</title>
+      </Head>
       <TopLeftImg />
       <Nav />
       <Header />
