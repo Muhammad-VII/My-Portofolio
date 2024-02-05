81985ad429b60ee0b0d10c5fa63876d35f177679
Mohammed Akmal <meshoronaldo62@gmail.com>
2024-02-05 17:18:09 +0200

    testing next meta data

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import { Sora } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+
 // font settings
 const sora = Sora({
   subsets: ["latin"],
@@ -10,21 +11,12 @@ const sora = Sora({
 import Nav from "./Nav";
 import Header from "./Header";
 import TopLeftImg from "./TopLeftImg";
-import Head from "next/head";
-
+ 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
       className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
     >
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta name="description" content="Akmal's Portfolio" />
-        <meta name="keywords" content="Akmal, Portfolio, Web Developer" />
-        <meta name="author" content="Mohmmed Akmal" />
-        <title>Akmal&rsquo;s Portfolio</title>
-      </Head>
       <TopLeftImg />
       <Nav />
       <Header />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,16 @@ import { useRouter } from "next/router";
 
 // framer motion
 import { AnimatePresence, motion } from "framer-motion";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: {
+    default: "Akmal's Portfolio",
+    template: "Akmal's Portfolio",
+  },
+  keywords: "Akmal, Portfolio, Web Developer",
+  description: 'This is Mohammed Akmal Portfolio which is made with Next.js and Tailwind CSS',
+}
 // pages
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
