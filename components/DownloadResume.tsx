5baa3d40febec7512053b7aeea203b3266f310e6
Mohammad Akmal <m.akmal@hqolsa.com>
2024-09-12 17:46:20 +0300

    updated resume

--- components/DownloadResume.tsx
+++ components/DownloadResume.tsx
@@ -5,7 +5,7 @@ import { AiOutlineDownload } from "react-icons/ai";
 const DownloadResume = () => {
   return (
     <a
-      href="/akmal_resume.pdf"
+      href="/akmal_latest_resume.pdf"
       download="Akmal's Resume"
       className="fixed bottom-28 right-[50%] translate-x-[50%] md:right-16 md:bottom-12 md:translate-x-0 p-4 bg-accent/70 text-white rounded-full shadow-lg animate-pulse cursor-pointer z-50 hover:bottom-16 transition-all"
     >
